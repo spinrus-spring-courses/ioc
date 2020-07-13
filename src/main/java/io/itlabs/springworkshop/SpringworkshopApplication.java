32063ea29f6ee450178a3bf6eab6fda4b47f358b
milinyh <milinyh@sdvor.com>
2020-07-13 20:46:07 +0500

    Инициализация

---
+++ src/main/java/io/itlabs/springworkshop/SpringworkshopApplication.java
@@ -0,0 +1,13 @@
+package io.itlabs.springworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringworkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringworkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/itlabs/springworkshop/SpringworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.itlabs.springworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringworkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
