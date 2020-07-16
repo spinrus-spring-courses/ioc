064c726b639e88b30369bced1f55f7a4952844dc
milinyh <milinyh@sdvor.com>
2020-07-16 21:24:39 +0500

    Добавил вооружение для персонажей

--- src/main/java/io/itlabs/springtraining/domain/person/Hobbit.java
+++ src/main/java/io/itlabs/springtraining/domain/person/Hobbit.java
@@ -2,7 +2,4 @@ package io.itlabs.springtraining.domain.person;
 
 public class Hobbit extends Person {
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/io/itlabs/springtraining/domain/person/Person.java
+++ src/main/java/io/itlabs/springtraining/domain/person/Person.java
@@ -1,13 +1,30 @@
 package io.itlabs.springtraining.domain.person;
 
 import io.itlabs.springtraining.domain.Named;
+import io.itlabs.springtraining.domain.weapon.Weapon;
+
+import java.util.List;
 
 public abstract class Person implements Named {
 
     protected String name;
 
+    protected List<Weapon> weapons;
+
     @Override
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(List<Weapon> weapons) {
+        this.weapons = weapons;
     }
 }
