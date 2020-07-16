a6e2805eefd7a4bbb4422f6620ed6c5d267023a8
milinyh <milinyh@sdvor.com>
2020-07-16 20:50:47 +0500

    Инициализация

--- src/main/java/io/itlabs/springworkshop/SpringTrainingApplication.java
+++ src/main/java/io/itlabs/springworkshop/SpringTrainingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringworkshopApplication {
+public class SpringTrainingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringworkshopApplication.class, args);
+        SpringApplication.run(SpringTrainingApplication.class, args);
     }
 
 }
