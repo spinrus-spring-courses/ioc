89871fde7d50b863aa0fda0b266242f2f7c8831d
milinyh <milinyh@sdvor.com>
2020-07-19 22:30:22 +0500

    Решение 4. Через сеттеры.

--- src/main/java/io/itlabs/springtraining/domain/groups/Shire.java
+++ src/main/java/io/itlabs/springtraining/domain/groups/Shire.java
@@ -11,20 +11,36 @@ import java.util.List;
 @Component
 public class Shire implements PersonGroup {
 
-    @Autowired
     private Person frodo;
 
-    @Autowired
     private Person sam;
 
-    @Autowired
     private Person meriadoc;
 
-    @Autowired
     private Person pippin;
 
     @Override
     public List<Person> persons() {
         return Arrays.asList(frodo, sam, meriadoc, pippin);
+    }
+
+    @Autowired
+    public void setFrodo(Person frodo) {
+        this.frodo = frodo;
+    }
+
+    @Autowired
+    public void setSam(Person sam) {
+        this.sam = sam;
+    }
+
+    @Autowired
+    public void setMeriadoc(Person meriadoc) {
+        this.meriadoc = meriadoc;
+    }
+
+    @Autowired
+    public void setPippin(Person pippin) {
+        this.pippin = pippin;
     }
 }
