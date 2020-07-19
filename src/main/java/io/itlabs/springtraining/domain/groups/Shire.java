475176309878d4f9662134a0ba43fc6b4b766dc6
milinyh <milinyh@sdvor.com>
2020-07-19 22:32:34 +0500

    Решение 4. Через инъекцию списка.

--- src/main/java/io/itlabs/springtraining/domain/groups/Shire.java
+++ src/main/java/io/itlabs/springtraining/domain/groups/Shire.java
@@ -1,46 +1,27 @@
 package io.itlabs.springtraining.domain.groups;
 
 import io.itlabs.springtraining.domain.PersonGroup;
+import io.itlabs.springtraining.domain.person.Hobbit;
 import io.itlabs.springtraining.domain.person.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class Shire implements PersonGroup {
 
-    private Person frodo;
+    private final List<Hobbit> hobbits;
 
-    private Person sam;
-
-    private Person meriadoc;
-
-    private Person pippin;
+    public Shire(List<Hobbit> hobbits) {
+        this.hobbits = hobbits;
+    }
 
     @Override
     public List<Person> persons() {
-        return Arrays.asList(frodo, sam, meriadoc, pippin);
-    }
-
-    @Autowired
-    public void setFrodo(Person frodo) {
-        this.frodo = frodo;
-    }
-
-    @Autowired
-    public void setSam(Person sam) {
-        this.sam = sam;
-    }
-
-    @Autowired
-    public void setMeriadoc(Person meriadoc) {
-        this.meriadoc = meriadoc;
-    }
-
-    @Autowired
-    public void setPippin(Person pippin) {
-        this.pippin = pippin;
+        return hobbits
+                .stream()
+                .map(Person.class::cast)
+                .collect(Collectors.toList());
     }
 }
