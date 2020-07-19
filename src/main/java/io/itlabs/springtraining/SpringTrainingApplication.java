410ed53dc9ae9be00e79a49283b4824e2165ab70
milinyh <milinyh@sdvor.com>
2020-07-19 20:38:30 +0500

    Добавил вывод в лог имена всех бинов типа Person

--- src/main/java/io/itlabs/springtraining/SpringTrainingApplication.java
+++ src/main/java/io/itlabs/springtraining/SpringTrainingApplication.java
@@ -1,15 +1,23 @@
 package io.itlabs.springtraining;
 
+import io.itlabs.springtraining.domain.person.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @ImportResource("classpath:spring-configuration.xml")
 public class SpringTrainingApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SpringTrainingApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringTrainingApplication.class, args);
+        final var applicationContext = SpringApplication.run(SpringTrainingApplication.class, args);
+        Stream.of(applicationContext.getBeanNamesForType(Person.class)).forEach(LOG::info);
     }
 
 }
