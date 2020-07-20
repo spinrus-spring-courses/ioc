1da22ac2e1a9e6227b7041a872fac7fa7827df14
milinyh <milinyh@sdvor.com>
2020-07-20 21:21:23 +0500

    Решение №6 - вывод в консоль

--- src/main/java/io/itlabs/springtraining/SpringTrainingApplication.java
+++ src/main/java/io/itlabs/springtraining/SpringTrainingApplication.java
@@ -1,8 +1,7 @@
 package io.itlabs.springtraining;
 
 import io.itlabs.springtraining.application.GreetingService;
-import io.itlabs.springtraining.domain.groups.Shire;
-import io.itlabs.springtraining.domain.weapon.Weapon;
+import io.itlabs.springtraining.domain.groups.FellowshipOfTheRing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -15,17 +14,8 @@ public class SpringTrainingApplication {
         final var applicationContext = SpringApplication.run(SpringTrainingApplication.class, args);
 
         final var greetingService = applicationContext.getBean("greetingService", GreetingService.class);
+        final var fellowshipOfTheRing = applicationContext.getBean("fellowshipOfTheRing", FellowshipOfTheRing.class);
 
-        final var shire = applicationContext.getBean("shire", Shire.class);
-        for (var person: shire.persons()) {
-            greetingService.greet(person);
-        }
-
-        final var dagger = applicationContext.getBean("dagger", Weapon.class);
-        dagger.setBroken(true);
-
-        for (var person: shire.persons()) {
-            greetingService.greet(person);
-        }
+        greetingService.greet(fellowshipOfTheRing.persons());
     }
 }
